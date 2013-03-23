3728d78a0f75f4980c32510c1c5a0c9f293a700b
Hieu Phan <hnphan@colby.edu>
2013-03-23 09:57:38 +0000

    Added unzip in marketplace.views

--- cerberusclient/marketplace/views.py
+++ cerberusclient/marketplace/views.py
@@ -11,6 +11,8 @@ import math
 import simplejson
 from django.db import models
 import time
+import zipfile
+
 
 
 PACKAGES_URL = 'http://localhost:8000/api/packages/'
@@ -150,6 +152,20 @@ def downloadChunks(url, package_object):
         return False
  
     return file_
-	
+
+
+def unzip(source_filename, dest_dir):
+    with zipfile.ZipFile(source_filename) as zf:
+        for member in zf.infolist():
+            # Path traversal defense copied from
+            # http://hg.python.org/cpython/file/tip/Lib/http/server.py#l789
+            words = member.filename.split('/')
+            path = dest_dir
+            for word in words[:-1]:
+                drive, word = os.path.splitdrive(word)
+                head, word = os.path.split(word)
+                if word in (os.curdir, os.pardir, ''): continue
+                path = os.path.join(path, word)
+            zf.extract(member, path)	
 
 
