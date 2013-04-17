9125be0ef4a13a318df2d789e52407c5d72e55a2
Hieu Phan <hnphan@colby.edu>
2013-04-17 14:18:03 +0100

    Installed bootmetro for cerberushost2

--- cerberushost2/config/urls.py
+++ cerberushost2/config/urls.py
@@ -2,12 +2,15 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
+import views
 
 from django.contrib import admin
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
+	url(r'^$', views.home, name='home'),
+	url(r'^api/', include('api.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
 )
