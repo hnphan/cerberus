cbc47e7c3aea4787a959f1af060f0bec51f87dc1
Hieu Phan <hnphan@colby.edu>
2013-04-19 00:22:07 +0100

    deleted unnecessary files

--- cerberusclient_old/marketplace/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from marketplace import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.home),
-    url(r'^(?P<package_id>\d+)/download/$', views.download),
-    url(r'^(?P<package_id>\d+)/$', views.package_detail, name='package_detail'),
-)
--- cerberushost_old/cerberushost/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-	context = 'dummy context'
-	return render(request,'index.html', context)
-
-
-
